bfc55234a321a72340f9011f4fa5a11eee4c855b
Forster112 <forstermichael112@gmail.com>
2022-06-01 20:44:32 +0000

    love python more than c

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for numbers in range (1, 101):
+    for numbers in range(1, 101):
         if numbers % 3 == 0 and numbers % 5 == 0:
             print("FizzBuzz ", end="")
         elif numbers % 5 == 0:
