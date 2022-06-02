d85be9493608275ae9c22790585e0adef647ff48
Forster112 <forstermichael112@gmail.com>
2022-06-02 21:10:22 +0100

    cant find the error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+from itertools import count
+
+
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
-    
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
