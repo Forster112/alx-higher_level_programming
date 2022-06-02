5676c2eabbf37347bf580549b5defa69ac0f3146
Forster112 <forstermichael112@gmail.com>
2022-06-02 20:33:10 +0100

    python is very interesting

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""My calculator functions
+
+Args:
+    a: first integer
+    b: second integer
+    
+Returns:
+    The return value.
+            a + b
+            a - b
+            a * b
+            a / b
+"""
+
+def add(a, b):
+    return (a + b)
+    
+    
+def add(a, b):
+    return (a - b)
+    
+    
+def add(a, b):
+    return (a * b)
+    
+    
+def add(a, b):
+    return int(a / b)
