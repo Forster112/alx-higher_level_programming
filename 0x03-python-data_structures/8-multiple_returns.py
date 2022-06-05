948e98d21f209f2af236cda8ded154b3352760b9
Forster112 <forstermichael112@gmail.com>
2022-06-05 15:58:22 +0100

    this should be it

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,4 +4,6 @@ def multiple_returns(sentence):
     if (len(sentence) == 0):
         sentence[0] = 'None'
     else:
-        return (len(sentence), sentence[0])
+        my_tuple = ()
+        my_tuple = len(sentence), sentence[0]
+        return my_tuple
