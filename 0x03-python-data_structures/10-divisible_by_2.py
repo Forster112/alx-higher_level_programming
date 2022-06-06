5932c37fdbca45e1af00c1bb7f51032435ce3abf
Forster112 <forstermichael112@gmail.com>
2022-06-06 08:58:32 +0100

    fixing again

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 def divisible_by_2(my_list=[]):
     numbers = my_list.copy()
     for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
+        if numbers[i] % 2 == 0:
             numbers[i] = True
         else:
             numbers[i] = False
