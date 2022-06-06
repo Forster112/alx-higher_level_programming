bee5df7968b17a233848989be9ea7b380ef102a6
Forster112 <forstermichael112@gmail.com>
2022-06-06 08:45:00 +0100

    switching between the two methods i know

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max_num = my_list[0]
     if len(my_list) == 0:
         return "None"
-    for i in range(my_list):
-        if my_list[i] > max_num:
-            max_num = my_list[i]
+    else:
+        max_num = max(my_list)
         return max_num
