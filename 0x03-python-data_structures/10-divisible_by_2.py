1bed3a8f98aa3084b7d47bc5d07c9361ad32e72b
Forster112 <forstermichael112@gmail.com>
2022-06-06 09:04:37 +0100

    a little mistake is all it takes

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     numbers = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            numbers[i].append(True)
+            numbers.append(True)
         else:
             numbers.append(False)
         return numbers
