645271b6dacfc891f01076274d43191f518ca85a
Forster112 <forstermichael112@gmail.com>
2022-06-20 12:51:12 +0100

    task 0 fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         print(*my_list[:x], sep="", end="")
         print()
-        return my_list[x - 1]
+        counter = 0
+        for i in my_list[:x]:
+            counter += 1
+        return counter
     except IndexError:
-        return my_list[-1]
+        return counter
