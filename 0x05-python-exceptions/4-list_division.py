c0c244bef5e2b77a8d4018895ae5f6f3e6daabf3
Forster112 <forstermichael112@gmail.com>
2022-06-20 20:19:20 +0100

    i dont know the requirement mistake

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,14 +4,14 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             divide = my_list_1[i]/ my_list_2[i]
-        except (TypeError, ValueError):
-            print('wrong type')
+        except TypeError:
+            print("wrong type")
             divide = 0
         except ZeroDivisionError:
-            print('division by 0')
+            print("division by 0")
             divide = 0
         except IndexError:
-            print('out of range')
+            print("out of range")
             divide = 0
         finally:
             answer.append(divide)
