c13333b462cb0fa0472905cff9ec25506d1b7e4b
Forster112 <forstermichael112@gmail.com>
2022-06-21 23:25:44 +0100

    fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,11 +26,11 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     @property
     def position(self):
         return self.__position
-    
+
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
