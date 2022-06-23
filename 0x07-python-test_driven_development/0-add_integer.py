7811daf03df162a817ebb621f3fde9a9c5f9a37f
Forster112 <forstermichael112@gmail.com>
2022-06-23 20:03:30 +0100

    trying the new test cases

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """A function that adds two integers
+
+    Args:
+        a (int/ float): integer to be added
+        b (int/ float): integer to be added with default value 98
+        """
+    
+    if not isinstance(a, int) or not isinstance(a, float):
+        """a must be an int or float else raise TypeError"""
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int) or not isinstance(b, float):
+        """b must be an int or float else raise TypeError"""
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
