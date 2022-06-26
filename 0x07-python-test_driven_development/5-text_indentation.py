75800c63a6778547512beebde04c63f656924564
Forster112 <forstermichael112@gmail.com>
2022-06-26 23:07:33 +0100

    this is shit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,4 +8,10 @@ def text_indentation(text):
         for i in text:
             if i == '.' or i == ':' or i == '?':
                 print()
-                break
+                if i == len(text) - 1:
+                    break
+                if i + 1 == ' ':
+                    i += 1
+                while i == ' ' and i+1 == ' ' and i+1 < len(text):
+                    i += 1
+            i += 1
