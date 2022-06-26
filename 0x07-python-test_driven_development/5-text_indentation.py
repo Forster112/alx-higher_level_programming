54e8eefef813704ac812db3b5a17e7e25f5a64d6
Forster112 <forstermichael112@gmail.com>
2022-06-26 22:51:55 +0100

    check

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,10 +3,8 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     else:
+        text = text.strip()
+        print(text)
         for i in text:
-            if i == ':' or i == "?" or i == '.':
-                print(text)
+            if i == '.' or i == ':' or i == '?':
                 print()
-                print()
-            else:
-                print(text)
