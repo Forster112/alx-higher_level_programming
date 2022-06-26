6c854ae631766b9a6e0ddc08bbd3935f91a7c6a4
Forster112 <forstermichael112@gmail.com>
2022-06-26 14:41:34 +0100

    fixing

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """Function that prints out a full name with first name and last name,
+        with the string e.g``My name is Michael Forster``.
+        Michael as first name and Forster as last name
+
+        Args:
+            first_name(string): first name of user must be string else raise TypeError
+            last_name(string): last name of user must be string else raise TypeError
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    my_name = print("My name is {} {}".format(first_name, last_name))
-    return my_name
+    print("My name is {} {}".format(first_name, last_name))
