c548477e148f71f3a45f3396d87c7eb2aa52e2d8
Forster112 <forstermichael112@gmail.com>
2022-06-26 23:13:00 +0100

    i will just do it

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,14 +4,15 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     else:
         text = text.strip()
-        print(text)
-        for i in text:
-            if i == '.' or i == ':' or i == '?':
+        i = 0
+        while i < len(text):
+            print(text[i], end="")
+            if text[i] == '.' or text[i] == ':' or text[i] == '?':
                 print()
                 if i == len(text) - 1:
                     break
-                if i + 1 == ' ':
+                if text[i + 1] == ' ':
                     i += 1
-                while i == ' ' and i+1 == ' ' and i+1 < len(text):
+                while text[i] == ' ' and text[i+1] == ' ' and i+1 < len(text):
                     i += 1
             i += 1
