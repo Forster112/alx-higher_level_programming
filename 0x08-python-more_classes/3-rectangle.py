cddba6d1e16495554f533c0b6eaa87f3f311516b
Forster112 <forstermichael112@gmail.com>
2022-06-28 00:07:26 +0100

    hope this

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -56,6 +56,6 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 print("#", end="")
-            if i is not self.__height -1:
+            if i != self.__height - 1:
                 print()
         return ""
