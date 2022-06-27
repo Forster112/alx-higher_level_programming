1aabe4fe7aad3319321e988ef286559e823b03cf
Forster112 <forstermichael112@gmail.com>
2022-06-27 23:24:49 +0100

    whats the problem now

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,11 +50,9 @@ class Rectangle:
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            self.__height = 0
-            self.__width = 0
-            print("")
+            return ("")
         for i in range(self.__height):
             for j in range(self.__width):
                 print("#", end="")
             print()
-        return ("")
+        return ""
