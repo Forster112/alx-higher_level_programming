05e9234d81cd229c1073b9f95641fd7790b4d498
Forster112 <forstermichael112@gmail.com>
2022-06-27 15:42:09 +0100

    new task

---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,9 @@
+# 0x08-python-more_classes
+
+
+
+
+## Tasks
+| File | Prototype or Class | Duty |
+| ---- | ------------------ | ---- |
+| [0-rectangle.py](0-rectangle.py) | Class Rectangle: | A class that does nothing |
---
+++ tryb.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+def is_week_day(day):
+    if day.weekday() == 5 or day.weekday() == 6:
+        return False
+    return True
+
+def main():
+    my_date = datetime.date(2022, 6, 25)
+    if is_week_day(my_date):
+        print("Today is {}, are you ready to work?". format(my_date))
+    else:
+        print("just rest")
+
+main()
