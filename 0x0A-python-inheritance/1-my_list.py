55278d5a2eb4f0508076808ae8c86b863d664028
Forster112 <forstermichael112@gmail.com>
2022-07-04 20:33:22 +0100

    task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""MyList that inherits from list"""
+
+
+class Mylist(list):
+    """A class that inherits from list"""
+    def print_sorted(self):
+        """A function that prints a sorted list"""
+        print(sorted(self))
