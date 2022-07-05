80573e415c207ffb936ed22fd9648cbf05c7b190
Forster112 <forstermichael112@gmail.com>
2022-07-05 10:45:32 +0100

    pycodestyle trouble

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -18,7 +18,7 @@ class Square(Rectangle):
     def area(self):
         """calculates the area of the square"""
         return self.__size * self.__size
-    
+
     def __str__(self):
         st = f"[{str(self.__class__.__name__)}] {str(self.__size)}"
         st += f"/{str(self.__size)}"
