2e58dd83dcabe40c108d12cab94bb9a721bb9117
Forster112 <forstermichael112@gmail.com>
2022-07-05 22:35:35 +0100

    will this work?

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
-from sys import argv
 
+
+
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-"""adds argument to a python list and save to a file"""
-new_list = []
-filename = "add_item.json"
-
-for i in argv[1:]:
-    new_list.append(i)
-save_to_json_file(new_list, filename)
-load_from_json_file(filename)
+    try:
+        new_list = load_from_json_file("add_item.json")
+    except:
+        pass
+        new_list = []
+    for i in argv[1:]:
+        new_list.append(i)
+    save_to_json_file(new_list, "add_item.json")
