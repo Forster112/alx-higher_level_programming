2d2b1a8e58f890a83232072373141e94ae74d774
Forster112 <forstermichael112@gmail.com>
2022-07-05 16:18:57 +0100

    try again

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines a write file function"""
 
+
 def write_file(filename="", text=""):
     """function that writes to a file
 
@@ -9,5 +10,4 @@ def write_file(filename="", text=""):
             if none new one is created
         text: the text to write"""
     with open(filename, encoding="utf-8") as f:
-        write_file = f.write(text)
-        return write_file
+        f.write(text)
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -6,3 +6,4 @@
 | module | function | duty |
 | ------ | -------- | ---- |
 | [0-read_file.py](0-read_file.py) | def read_file(filename=""): | function that reads utf-8 text file |
+| [1-write_file.py](1-write_file.py) | def write_file(filename="", text=""): | writes a string to a file |
