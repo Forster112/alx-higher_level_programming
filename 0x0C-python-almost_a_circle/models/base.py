b14880c4eb115b04d3683c71fcd2e8acf7e55ee9
Forster112 <forstermichael112@gmail.com>
2022-07-12 18:42:44 +0100

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """static method that returns json string
             representation of a list dictionary
@@ -41,3 +42,11 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation"""
+        if not json_string:
+            return []
+        else:
+            return json.loads(json_string)
