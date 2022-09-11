cc991b6d617808b3b2421454c6ad3ed52c31f6ed
Forster112 <forstermichael112@gmail.com>
2022-09-11 14:15:13 +0100

    according to argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,7 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], stname=sys.argv[4],
-                         port=3306)
+                         db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
             WHERE CONVERT(`name` USING Latin1) \
