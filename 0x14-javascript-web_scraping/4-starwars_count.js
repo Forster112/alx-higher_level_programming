7cf6fe793c217f8659863ba9083185e846ce4bf8
Forster112 <forstermichael112@gmail.com>
2022-10-20 18:01:14 +0100

    18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,22 @@
 #!/usr/bin/node
+const reqargs = process.argv[2];
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const result = JSON.parse(body).result;
-    console.log(result.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endswith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
+request(reqargs, function (error, response, body) {
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+  } else {
+    const jso = JSON.parse(body);
+    const results = jso.results;
+    let count = 0;
+    for (let i = 0; i < results.length; i++) {
+      const chars = (results[i].characters);
+      for (let j = 0; j < chars.length; j++) {
+        const check18 = chars[j].endsWith('18/');
+        if (check18) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
